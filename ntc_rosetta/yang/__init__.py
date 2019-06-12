b59e84e984da01adca755d015d6d226709c70aed
David Barroso <dbarrosop@dravetech.com>
2019-06-12 19:27:51 +0200

    expose paths so they can be consumed from the outside

--- ntc_rosetta/yang/__init__.py
+++ ntc_rosetta/yang/__init__.py
@@ -3,30 +3,32 @@ import pathlib
 from yangson.datamodel import DataModel
 
 
+base = pathlib.Path(__file__).parent
+openconfig_lib = f"{base}/openconfig.json"
+openconfig_path = [
+    base.joinpath("YangModels/standard/ietf/RFC"),
+    base.joinpath("openconfig/release/models"),
+    base.joinpath("openconfig/release/models/acl"),
+    base.joinpath("openconfig/release/models/aft"),
+    base.joinpath("openconfig/release/models/bgp"),
+    base.joinpath("openconfig/release/models/interfaces"),
+    base.joinpath("openconfig/release/models/isis"),
+    base.joinpath("openconfig/release/models/mpls"),
+    base.joinpath("openconfig/release/models/multicast"),
+    base.joinpath("openconfig/release/models/network-instance"),
+    base.joinpath("openconfig/release/models/local-routing"),
+    base.joinpath("openconfig/release/models/ospf"),
+    base.joinpath("openconfig/release/models/policy"),
+    base.joinpath("openconfig/release/models/policy-forwarding"),
+    base.joinpath("openconfig/release/models/rib"),
+    base.joinpath("openconfig/release/models/segment-routing"),
+    base.joinpath("openconfig/release/models/types"),
+    base.joinpath("openconfig/release/models/vlan"),
+]
+
+
 def _get_openconfig_data_model() -> DataModel:
-    base = pathlib.Path(__file__).parent
-    lib = f"{base}/openconfig.json"
-    path = [
-        base.joinpath("YangModels/standard/ietf/RFC"),
-        base.joinpath("openconfig/release/models"),
-        base.joinpath("openconfig/release/models/acl"),
-        base.joinpath("openconfig/release/models/aft"),
-        base.joinpath("openconfig/release/models/bgp"),
-        base.joinpath("openconfig/release/models/interfaces"),
-        base.joinpath("openconfig/release/models/isis"),
-        base.joinpath("openconfig/release/models/mpls"),
-        base.joinpath("openconfig/release/models/multicast"),
-        base.joinpath("openconfig/release/models/network-instance"),
-        base.joinpath("openconfig/release/models/local-routing"),
-        base.joinpath("openconfig/release/models/ospf"),
-        base.joinpath("openconfig/release/models/policy"),
-        base.joinpath("openconfig/release/models/policy-forwarding"),
-        base.joinpath("openconfig/release/models/rib"),
-        base.joinpath("openconfig/release/models/segment-routing"),
-        base.joinpath("openconfig/release/models/types"),
-        base.joinpath("openconfig/release/models/vlan"),
-    ]
-    return DataModel.from_file(lib, path)
+    return DataModel.from_file(openconfig_lib, openconfig_path)
 
 
 def get_data_model(model: str = "openconfig") -> DataModel:
