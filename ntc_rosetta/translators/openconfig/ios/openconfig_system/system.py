b2daf5238e787e4dce72b531ecb2c7f8f8858dd6
Jeff Emershaw <me@jeffemershaw.com>
2019-06-13 11:56:48 -0400

    Adding ntp and dns config and started on merge

--- ntc_rosetta/translators/openconfig/ios/openconfig_system/system.py
+++ ntc_rosetta/translators/openconfig/ios/openconfig_system/system.py
@@ -11,19 +11,28 @@ class DnsServerConfig(Translator):
     class Yangify(TranslatorData):
         path = "openconfig-system:system/dns/servers/server/config"
 
+
 class DnsServer(Translator):
     config = DnsServerConfig
 
     class Yangify(TranslatorData):
         path = "openconfig-system:system/dns/servers/server"
 
-    address = unneeded
+        def pre_process_list(self) -> None:
+            if self.to_remove:
+                for element in self.to_remove:
+                    self.result.add_command(f"no dns server {element['address']}")
+
+    def address(self, value: str):
+        if value:
+            self.yy.result.add_command(f"dns server {value}")
 
 class DnsServers(Translator):
     server = DnsServer
 
     class Yangify(TranslatorData):
         path = "openconfig-system:system/dns/servers"
+
 
         def pre_process(self) -> None:
             pass
@@ -35,15 +44,40 @@ class Dns(Translator):
     config = DnsConfig
     servers = DnsServers
 
+class NtpServerConfig(Translator):
+    class Yangify(TranslatorData):
+        path = "openconfig-system:system/ntp/servers/server/config"
+
+class NtpServer(Translator):
+    config = NtpServerConfig
+
+    class Yangify(TranslatorData):
+        path = "openconfig-system:system/ntp/servers/server"
+
+        def pre_process_list(self) -> None:
+            if self.to_remove:
+                for element in self.to_remove:
+                    self.result.add_command(f"no ntp server {element['address']}")
+
+    def address(self, value: str):
+        self.yy.result.add_command(f"ntp server {value} prefer")
+
+class NtpServers(Translator):
+    server = NtpServer
+
+    class Yangify(TranslatorData):
+        path = "openconfig-system:system/ntp/servers"
+
 class NtpConfig(Translator):
     class Yangify(TranslatorData):
-        path = "openconfig-system:system/ntp"
+        path = "openconfig-system:system/ntp/config"
 
 class Ntp(Translator):
     class Yangify(TranslatorData):
         path = "openconfig-system:system/ntp"
 
     config = NtpConfig
+    servers = NtpServers
 
 class SshServerConfig(Translator):
     class Yangify(TranslatorData):
@@ -71,11 +105,39 @@ class AaaAuthenticationUserConfig(Translator):
     class Yangify(TranslatorData):
         path = "openconfig-system:system/aaa/authentication/users/user/config"
 
+        def pre_process(self) -> None:
+            self.extra = {
+                    "username": "",
+                    "password": "",
+                    "password_hash": "",
+                    "role": "",
+                    "ssh_key": ""
+            }
+
+        def post_process(self) -> None:
+            extra = self.extra
+            if not extra['username'] or not extra['role'] or not extra['password_hash']:
+                return None
+            self.result.add_command("username {} privilege {} secret {}".format(extra['username'], extra['role'], extra['password_hash']))
+
     def username(self, value: str):
-        print(value)
-        print(jh.query('config', self.yy.candidate["openconfig-system:system"]))
-        # print(self.yy.candidate['openconfig-system:system']['aaa'])
-        # print(dir(self.yy.root_result))
+        self.yy.extra['username'] = value
+        return None
+
+    def role(self, value: str):
+        self.yy.extra['role'] = value
+        return None
+
+    def password(self, value: str):
+        self.yy.extra['password'] = value
+        return None
+
+    def password_hashed(self, value: str):
+        self.yy.extra['password_hash'] = value
+        return None
+
+    def ssh_key(self, value: str):
+        self.yy.extra['ssh_key'] = value
         return None
 
 class AaaAuthenticationUser(Translator):
@@ -122,3 +184,5 @@ class System(Translator):
     config = SystemConfig
     ssh_server = SshServer
     aaa = Aaa
+    ntp = Ntp
+    dns = Dns
