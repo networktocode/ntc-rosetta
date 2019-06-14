ba0aa0ae2972bde1bd413c56c95a800587b1ae74
Daniel <djustice@wificidr.net>
2019-06-14 09:07:57 -0500

    Added check for integer vlan

--- ntc_rosetta/parsers/openconfig/junos/helpers.py
+++ ntc_rosetta/parsers/openconfig/junos/helpers.py
@@ -6,6 +6,9 @@ def resolve_vlan_ids(vlans: List, root) -> List[int]:
     """Resolve named vlans from /configuration/vlans/vlan path"""
     rv = list()
     for vlan in vlans:
+        if vlan.text.isnumeric():
+            rv.append(int(vlan.text))
+            continue
         elem = root["dev_conf"].xpath(
             "//vlan[name/text()='{member}']/vlan-id".format(member=vlan.text)
         )
