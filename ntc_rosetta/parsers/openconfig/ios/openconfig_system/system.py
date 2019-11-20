05731c7dcb3de8e0e943c2a485778ac68ea6fac0
Packet Netarch <netarch@packet.com>
2019-11-20 14:47:16 -0600

    Implement openconfig-system for IOS

---
+++ ntc_rosetta/parsers/openconfig/ios/openconfig_system/system.py
@@ -0,0 +1,309 @@
+from typing import Any, Dict, Iterator, Tuple
+
+from ntc_rosetta.helpers import json_helpers as jh
+
+from yangify.parser import Parser, ParserData
+
+
+import re
+
+
+class ClockConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/clock/config"
+
+    def timezone_name(self) -> str:
+        v = jh.query('clock.timezone."#text"', self.yy.native)
+        if v is not None:
+            return str(v)
+        return None
+
+
+class Clock(Parser):
+    config = ClockConfig
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/clock"
+
+
+class DnsConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/dns/config"
+
+    def search(self) -> str:
+        return None
+
+
+class DnsServerConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/dns/servers/server/config"
+
+    def address(self) -> str:
+        return str(self.yy.key)
+
+    def port(self) -> int:
+        return 53
+
+
+class DnsServer(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/dns/servers/server"
+
+        def extract_elements(self) -> Iterator[Tuple[str, Dict[str, Any]]]:
+            dns = jh.query('ip."name-server"', self.native) or {}
+            for k, v in dns.items():
+                if k == "#text":
+                    continue
+                yield k, v
+
+    def address(self) -> str:
+        return str(self.yy.key)
+
+    config = DnsServerConfig
+
+
+class DnsServers(Parser):
+    server = DnsServer
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/dns/servers"
+
+
+class Dns(Parser):
+    config = DnsConfig
+    servers = DnsServers
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/dns"
+
+
+class NtpConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/ntp/config"
+
+    def enabled(self) -> bool:
+        if jh.query("ntp", self.yy.native):
+            return True
+        return False
+
+    def ntp_source_address(self) -> str:
+        return None
+
+    def enable_ntp_auth(self) -> bool:
+        if jh.query("ntp.authenticate", self.yy.native):
+            return True
+        return False
+
+
+class NtpServerConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/ntp/servers/server/config"
+
+    def address(self) -> str:
+        return str(self.yy.key)
+
+
+class NtpServer(Parser):
+    config = NtpServerConfig
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/ntp/servers/server"
+
+        def extract_elements(self) -> Iterator[Tuple[str, Dict[str, Any]]]:
+            ntp_servers = jh.query("ntp.server", self.native) or {}
+            for k, v in ntp_servers.items():
+                if k == "#text":
+                    continue
+                yield k, v
+
+    def address(self) -> str:
+        return str(self.yy.key)
+
+
+class NtpServers(Parser):
+    server = NtpServer
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/ntp/servers"
+
+
+class Ntp(Parser):
+    config = NtpConfig
+    servers = NtpServers
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/ntp"
+
+
+class SshServerConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/ssh-server/config"
+
+    def enable(self) -> bool:
+        # NOTE: This is always true
+        return True
+
+    def protocol_version(self) -> str:
+        version = jh.query('ip.ssh.version."#text"', self.yy.native)
+
+        if version and version == "2":
+            return "V2"
+        elif version and version == "1":
+            return "V1"
+        return "V1_V2"
+
+    def timeout(self) -> int:
+        timeout = jh.query('ip.ssh."time-out"."#text"', self.yy.native)
+        if timeout:
+            return timeout
+        return None
+
+
+class SshServer(Parser):
+    config = SshServerConfig
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/ssh-server"
+
+
+class TelnetServerConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/telnet-server/config"
+
+    def enable(self) -> bool:
+        # TODO: I believe this is always enabled, but need a way to check if it
+        # is listening on the tty ports
+        return True
+
+    def timeout(self) -> int:
+        return None
+
+
+class TelnetServer(Parser):
+    config = TelnetServerConfig
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/telnet-server"
+
+
+class AaaAuthenticationUserConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/aaa/authentication/users/user/config"
+
+    def username(self) -> str:
+        return str(self.yy.key)
+
+    def role(self) -> str:
+        text = jh.query('"#text"', self.yy.native)
+
+        role_RE = r"privilege\s(\d+)(?:\ssecret|\spassword)"
+        result = re.match(role_RE, text)
+
+        if result:
+            return result[1]
+        return None
+
+    def password(self) -> str:
+        text = jh.query('"#text"', self.yy.native)
+        password_RE = r".*password ((\d) )?(.*)"
+        result = re.match(password_RE, text)
+
+        if result:
+            _, _, password = result.groups()
+            return password
+        return None
+
+    def password_hashed(self) -> str:
+        text = jh.query('"#text"', self.yy.native)
+        password_RE = r".*secret ((\d) )?(.*)"
+        result = re.match(password_RE, text)
+
+        if result:
+            _, _, secret = result.groups()
+            return secret
+        return None
+
+    def ssh_key(self) -> str:
+        return None
+
+
+class AaaAuthenticationUser(Parser):
+    config = AaaAuthenticationUserConfig
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/aaa/authentication/users/user"
+
+        def extract_elements(self) -> Iterator[Tuple[str, Dict[str, Any]]]:
+            users = jh.query("username", self.native) or {}
+            for k, v in users.items():
+                if k == "#text":
+                    continue
+                yield k, v
+
+    def username(self) -> str:
+        return str(self.yy.key)
+
+
+class AaaAuthenticationUsers(Parser):
+    user = AaaAuthenticationUser
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/aaa/authentication/users"
+
+
+class AaaAuthentication(Parser):
+    users = AaaAuthenticationUsers
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/aaa/authentication"
+
+
+class Aaa(Parser):
+    authentication = AaaAuthentication
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/aaa"
+
+
+class SystemConfig(Parser):
+    class Yangify(ParserData):
+        path = "/openconfig-system:system/config"
+
+    def hostname(self) -> str:
+        v = jh.query('hostname."#text"', self.yy.native)
+        if v is not None:
+            return str(v)
+        else:
+            return None
+
+    def domain_name(self) -> str:
+        v = jh.query('ip."domain-name"."#text"', self.yy.native)
+        if v is not None:
+            return str(v)
+        else:
+            return None
+
+    def login_banner(self) -> str:
+        # TODO
+        return None
+
+    def motd_banner(self) -> str:
+        # TODO
+        return None
+
+
+class System(Parser):
+    config = SystemConfig
+    clock = Clock
+    dns = Dns
+    ntp = Ntp
+    ssh_server = SshServer
+    telnet_server = TelnetServer
+    aaa = Aaa
+
+    class Yangify(ParserData):
+        path = "/openconfig-system:system"
+        metadata = {"key": "dev_conf", "command": "show running-config all"}
+
+        def pre_process(self) -> None:
+            self.native: Dict[str, Any] = self.root_native["dev_conf"]
---
+++ ntc_rosetta/translators/openconfig/ios/openconfig_system/system.py
@@ -0,0 +1,228 @@
+import re
+
+from yangify.translator import Translator, TranslatorData
+
+from yangson.exceptions import InstanceValueError
+
+
+class DnsConfig(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/dns/config"
+
+
+class DnsServerConfig(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/dns/servers/server/config"
+
+
+class DnsServer(Translator):
+    config = DnsServerConfig
+
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/dns/servers/server"
+
+        def pre_process_list(self) -> None:
+            if self.to_remove and not self.replace:
+                for element in self.to_remove:
+                    self.result.add_command(f"no ip name-server {element['address']}")
+
+    def address(self, value: str):
+        self.yy.result.add_command(f"ip name-server {value}")
+
+
+class DnsServers(Translator):
+    server = DnsServer
+
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/dns/servers"
+
+        def pre_process(self) -> None:
+            pass
+
+
+class Dns(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/dns"
+
+    config = DnsConfig
+    servers = DnsServers
+
+
+class NtpServerConfig(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/ntp/servers/server/config"
+
+
+class NtpServer(Translator):
+    config = NtpServerConfig
+
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/ntp/servers/server"
+
+        def pre_process_list(self) -> None:
+            if self.to_remove:
+                for element in self.to_remove:
+                    self.result.add_command(f"no ntp server {element['address']}")
+
+    def address(self, value: str):
+        self.yy.result.add_command(f"ntp server {value}")
+
+
+class NtpServers(Translator):
+    server = NtpServer
+
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/ntp/servers"
+
+
+class NtpConfig(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/ntp/config"
+
+
+class Ntp(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/ntp"
+
+    config = NtpConfig
+    servers = NtpServers
+
+
+class SshServerConfig(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/ssh-server/config"
+
+    def protocol_version(self, value: str) -> None:
+        if value == "V2":
+            self.yy.result.add_command(f"ip ssh version 2")
+        elif value == "V1":
+            self.yy.result.add_command(f"ip ssh version 1")
+        else:
+            self.yy.result.add_command(f"default ip ssh version")
+
+    def timeout(self, value: int) -> None:
+        if value:
+            self.yy.result.add_command(f"ip ssh time-out {value}")
+
+
+class SshServer(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/ssh-server"
+
+    config = SshServerConfig
+
+
+class AaaAuthenticationUserConfig(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/aaa/authentication/users/user/config"
+
+        def pre_process(self) -> None:
+            self.extra = {
+                "username": "",
+                "password": "",
+                "password_hash": "",
+                "role": "",
+                "ssh_key": "",
+            }
+
+        def post_process(self) -> None:
+            if not self.extra.get("username"):
+                return
+            role = self.extra.get("role")
+            role_str = f" privilege {role}" if role else ""
+            password = self.extra.get("password")
+            password_str = f"password 0 {password}" if password else ""
+            password_hash = self.extra.get("password_hash")
+            match = re.match(r"^\$(\d).*", password_hash)
+            password_hash_str = ""
+            if match:
+                hash_type = match.groups()[0]
+                password_hash_str = f"secret {hash_type} {password_hash}"
+            ssh_key = self.extra.get("ssh_key")
+            ssh_key_str = f"ssh_key {ssh_key}" if ssh_key else ""  # noqa
+            self.result.add_command(
+                f"username {self.extra['username']}{role_str} {password_str or password_hash_str}"
+            )
+            if ssh_key:
+                """
+                ip ssh pubkey-chain
+                  username test
+                    key-string
+                      some-rsa-ssh-key-string
+                    exit
+                  exit
+                """
+                raise NotImplementedError
+
+    def username(self, value: str) -> None:
+        self.yy.extra["username"] = value
+
+    def role(self, value: str) -> None:
+        self.yy.extra["role"] = value
+
+    def password(self, value: str) -> None:
+        self.yy.extra["password"] = value
+
+    def password_hashed(self, value: str) -> None:
+        self.yy.extra["password_hash"] = value
+
+    def ssh_key(self, value: str) -> None:
+        self.yy.extra["ssh_key"] = value
+
+
+class AaaAuthenticationUser(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/aaa/authentication/users/user"
+
+        def pre_process_list(self) -> None:
+            if self.to_remove:
+                for element in self.to_remove:
+                    self.result.add_command(f"no username {element['username']}")
+
+    config = AaaAuthenticationUserConfig
+
+
+class AaaAuthenticationUsers(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/aaa/authentication/users"
+
+    user = AaaAuthenticationUser
+
+
+class AaaAuthentication(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/aaa/authentication"
+
+    users = AaaAuthenticationUsers
+
+
+class Aaa(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/aaa"
+
+    authentication = AaaAuthentication
+
+
+class SystemConfig(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system/config"
+
+    def hostname(self, value: str) -> None:
+        self.yy.result.add_command(f"hostname {value}")
+
+
+class System(Translator):
+    class Yangify(TranslatorData):
+        path = "/openconfig-system:system"
+
+        def pre_process(self):
+            if self.replace:
+                raise InstanceValueError(
+                    self.path, "Replace is not a supported operation for this path"
+                )
+
+    config = SystemConfig
+    ssh_server = SshServer
+    aaa = Aaa
+    ntp = Ntp
+    dns = Dns
